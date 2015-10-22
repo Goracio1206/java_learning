c0fa613bee16b6f6d41b9399d6f44eca2a289d8e
Vitalii.Nedzelskyi <vitalii.nedzelskyi@gmail.com>
2015-10-22 17:44:07 +0300

    master

--- src/Facultativ/Classes.java
+++ src/Facultativ/Classes.java
@@ -6,7 +6,18 @@ package Facultativ;
  * которая сохраняется в Архиве. Студентов, Преподавателей и Курсов при обучении может быть несколько.
  */
 public class Classes {
-    private String facultativ;
+    private String classesName;
+    private int point;
 
+    public Classes(String classesName) {
+        this.classesName = classesName;
+    }
+
+    public Classes(String classesName, int point) {
+        this.classesName = classesName;
+        this.point = point;
+    }
+
+    public Classes() {}
 
 }
