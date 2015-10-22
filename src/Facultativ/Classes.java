7bc9292e7d351cc3f8beca5fdbae794b1c64891d
Vitalii.Nedzelskyi <vitalii.nedzelskyi@gmail.com>
2015-10-22 17:45:37 +0300

    master

--- src/Facultativ/Classes.java
+++ src/Facultativ/Classes.java
@@ -20,4 +20,19 @@ public class Classes {
 
     public Classes() {}
 
+    public String getClassesName() {
+        return classesName;
+    }
+
+    public void setClassesName(String classesName) {
+        this.classesName = classesName;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
 }
