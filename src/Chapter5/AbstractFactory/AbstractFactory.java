e78170abb33b25af6bfe24336f4087e26ec6d239
Vitalii.Nedzelskyi <vitalii.nedzelskyi@gmail.com>
2015-11-04 17:14:22 +0200

    AbstractFactory

--- src/Chapter5/AbstractFactory/AbstractFactory.java
+++ src/Chapter5/AbstractFactory/AbstractFactory.java
@@ -4,7 +4,7 @@ package Chapter5.AbstractFactory;
  * Created by Vitalii.Nedzelskyi on 04.11.2015.
  */
 public class AbstractFactory {
-    enum Color {WHITE, BLACK};
+    enum Color {WHITE, BLACK}
     public static BaseFactory getFactory(String data) {
         Color my = Color.valueOf(data.toUpperCase());
         switch(my) {
