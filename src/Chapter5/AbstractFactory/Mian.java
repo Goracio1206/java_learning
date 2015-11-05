ea4a668617b59d9add49d58dd3f61fbe032819fe
Admin <vitalii.nedzelskyi@gmail.com>
2015-11-05 20:38:43 +0200

    Update

--- src/Chapter5/AbstractFactory/Mian.java
+++ src/Chapter5/AbstractFactory/Mian.java
@@ -11,13 +11,13 @@ public class Mian {
         BaseFactory fact1 = AbstractFactory.getFactory("White");
         BaseFactory fact2 = AbstractFactory.getFactory("Black");
         Circle circle1 = fact1.createCircle(4.6);
-        Circle cercle2 = fact2.createCircle(5.7);
+        Circle circle = fact2.createCircle(5.7);
         Triangle tri1 = fact1.createTriangle(4.1, 5.2);
         Triangle tri2 = fact2.createTriangle(4.2, 5.4);
 
         tri1.square();
         tri2.square();
         circle1.square();
-        cercle2.square();
+        circle.square();
     }
 }
