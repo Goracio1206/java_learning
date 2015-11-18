ea08bedada08bb21f2c6e89d2c0c3d7a717b4402
Vitalii.Nedzelskyi <vitalii.nedzelskyi@gmail.com>
2015-11-18 19:02:26 +0200

    Chapter6

---
+++ src/HomeWork/GolovachCourses/Thread_Runnable/RapidAtak.java
@@ -0,0 +1,12 @@
+package HomeWork.GolovachCourses.Thread_Runnable;
+
+/**
+ * Created by Vitalii.Nedzelskyi on 18.11.2015.
+ */
+public class RapidAtak  {
+    public static void main(String[] args) {
+
+    }
+}
+
+
