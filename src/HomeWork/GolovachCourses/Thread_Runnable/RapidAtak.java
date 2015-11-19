15ecb50240ac0feb4b9269925fe6660d2e0917ca
Vitalii.Nedzelskyi <vitalii.nedzelskyi@gmail.com>
2015-11-19 19:21:55 +0200

    Chapter6

--- src/HomeWork/GolovachCourses/Thread_Runnable/RapidAtak.java
+++ src/HomeWork/GolovachCourses/Thread_Runnable/RapidAtak.java
@@ -5,8 +5,39 @@ package HomeWork.GolovachCourses.Thread_Runnable;
  */
 public class RapidAtak  {
     public static void main(String[] args) {
-
+        Thread thread = new Thread(new RapidFactory(1000));
+        thread.start();
     }
 }
+
+class RapidFactory implements Runnable {
+
+    private long sleep;
+
+    public RapidFactory(long sleep) {
+        this.sleep = sleep;
+    }
+
+    public void run() {
+       while (true) {
+           creator();
+       }
+    }
+
+    private void creator() {
+        for (int i = 0; i <2 ; i++) {
+            try {
+                Thread thread = new Thread(new RapidFactory(sleep));
+                thread.start();
+                Thread.sleep(sleep);
+                System.out.println("hi!" + "  " + thread.getId() + " " + thread.getName());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
+
 
 
